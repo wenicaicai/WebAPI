34c3986a53a31e951f0edd23d1c6708728812f7d
wenicaicai <youngwenicai@outlook.com>
2020-01-08 18:08:34 +0800

    init

---
+++ myApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+            WriteLine("the current time is "+DateTime.Now);
+        }
+    }
+}
