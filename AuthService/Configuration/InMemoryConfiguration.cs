993a9d7fef87720d9a08646d50b086f72872a183
wenicaicai <youngwenicai@outlook.com>
2020-03-09 17:12:32 +0800

    1.Auth Service

---
+++ AuthService/Configuration/InMemoryConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+
+namespace AuthService.Configuration
+{
+    /// <summary>
+    /// 签名：一对公钥、秘钥
+    /// </summary>
+    public class InMemoryConfiguration
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[]
+            {
+                //{name:social-network,displayName:社交网络}
+                new ApiResource("socialnetwork","社交网络")
+            };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "socialnetwork",
+                    //用于
+                    ClientSecrets = new[] { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new[] { "socialnetwork" }
+                }
+            };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[]
+            {
+                //用于测试
+                new TestUser
+                {
+                    SubjectId ="1",
+                    Username = "1216040822@qq.com",
+                    Password = "2020"
+                }
+            };
+        }
+    }
+}
---
+++ AuthService/Startup.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthService.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AuthService
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //注册服务 AuthService
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddTestUsers(InMemoryConfiguration.Users().ToList())
+                .AddInMemoryClients(InMemoryConfiguration.Clients())
+                .AddInMemoryApiResources(InMemoryConfiguration.ApiResources());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //配置.net Core 管道 使用这个中间件
+                app.UseIdentityServer();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
